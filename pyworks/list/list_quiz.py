4afc70effa6416b28d0cd1109d68bc4234e6d0f1
marshmallow0-0 <possibleway23@gmail.com>
2024-08-02 19:27:42 +0900

    리스트

---
+++ pyworks/list/list_quiz.py
@@ -0,0 +1,12 @@
+rainbow = ['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'purple']
+#1.
+print(rainbow[2])
+#2.
+rainbow.sort()
+print(rainbow)
+#3.
+rainbow.append("skyblue")
+print(rainbow)
+#4.
+del rainbow[3:7]
+print(rainbow)
