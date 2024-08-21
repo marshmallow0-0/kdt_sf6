ac83f278d0f17f23417542ac6b654863465a320e
marshmallow0-0 <possibleWay23@gmail.com>
2024-08-21 20:54:20 +0900

    크롤링

--- pyworks/window_program/mydic.py
+++ pyworks/window_program/mydic.py
@@ -1,9 +1,14 @@
 # 컴퓨터 작은 사전 만들기
 from tkinter import *
 def click():
-    word = entry.get()
-    # dic[word]   #dic[key] -> value
-    text.insert(END, dic[word])
+    try:
+        word = entry.get()
+        definition = dic[word]
+        text.delete(0.0, END) #앞0-행, 뒤0-열
+        # text.insert(END, definition) #END - 최종 입력지점
+    except KeyError:
+        definition = "단어를 찾을 수 없습니다."
+    text.insert(END, definition)
 
 dic = {
     "비트": "0이나 1의 값을 가지는 컴퓨터 데이터의 최소 단위",
@@ -24,3 +29,4 @@ Button(root, text="제출", command=click).grid(row=2, column=0, sticky=W)
 text = Text(root, width=80, height=10, bg="light sky blue")
 text.grid(row=3, column=0, sticky=W)
 root.mainloop()
+
